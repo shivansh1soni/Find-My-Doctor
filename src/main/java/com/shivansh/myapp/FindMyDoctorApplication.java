4ea771e4cd1774ad160379f93b54b4f28b628ae3
Shivansh Soni <shivanshsoni568@gmail.com>
2024-07-04 08:27:07 +0530

    Main class

---
+++ src/main/java/com/shivansh/myapp/FindMyDoctorApplication.java
@@ -0,0 +1,13 @@
+package com.shivansh.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMyDoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMyDoctorApplication.class, args);
+	}
+
+}
