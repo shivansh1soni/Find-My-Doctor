bec5fb51ae48f8eece74d4f9429fe793fa993dbb
Shivansh Soni <shivanshsoni568@gmail.com>
2024-07-04 08:28:27 +0530

    Initial commit

---
+++ src/test/java/com/shivansh/myapp/FindMyDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivansh.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMyDoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
