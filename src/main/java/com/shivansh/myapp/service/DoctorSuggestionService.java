66c92b50b64160ffe1a1f6ae28d0aff121cec7cc
Shivansh Soni <shivanshsoni568@gmail.com>
2024-07-04 09:16:52 +0530

    added InvalidCity exception

--- src/main/java/com/shivansh/myapp/service/DoctorSuggestionService.java
+++ src/main/java/com/shivansh/myapp/service/DoctorSuggestionService.java
@@ -1,11 +1,15 @@
 package com.shivansh.myapp.service;
 
 import com.shivansh.myapp.entity.Doctor;
+import com.shivansh.myapp.entity.Doctor.Speciality;
 import com.shivansh.myapp.entity.Patient;
+import com.shivansh.myapp.exception.InvalidCityException;
 import com.shivansh.myapp.exception.NoDoctorFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+import static com.shivansh.myapp.entity.Doctor.Speciality.*;
 
 @Service
 public class DoctorSuggestionService {
@@ -19,28 +23,32 @@ public class DoctorSuggestionService {
         this.doctorService = doctorService;
     }
 
-    private Doctor.Speciality getSpecialityForSymptom(Patient.Symptom symptom) {
+    private Speciality getSpecialityForSymptom(Patient.Symptom symptom) {
         return switch (symptom) {
-            case ARTHRITIS, BACK_PAIN, TISSUE_INJURIES -> Doctor.Speciality.ORTHOPAEDIC;
-            case DYSMENORRHEA -> Doctor.Speciality.GYNECOLOGY;
-            case SKIN_INFECTION, SKIN_BURN -> Doctor.Speciality.DERMATOLOGY;
-            case EAR_PAIN -> Doctor.Speciality.ENT_SPECIALIST;
+            case ARTHRITIS, BACK_PAIN, TISSUE_INJURIES -> ORTHOPAEDIC;
+            case DYSMENORRHEA -> GYNECOLOGY;
+            case SKIN_INFECTION, SKIN_BURN -> DERMATOLOGY;
+            case EAR_PAIN -> ENT_SPECIALIST;
             default -> throw new IllegalArgumentException("Unknown symptom: " + symptom);
         };
     }
 
-    public List<Doctor> suggestDoctors(Long patientId) throws NoDoctorFoundException {
+    public List<Doctor> suggestDoctors(Long patientId) throws NoDoctorFoundException, InvalidCityException {
         Patient patient = patientService.getPatientById(patientId);
-        Doctor.Speciality requiredSpeciality = getSpecialityForSymptom(patient.getSymptom());
+        Speciality requiredSpeciality = getSpecialityForSymptom(patient.getSymptom());
 
-        List<Doctor> doctors = doctorService.getDoctorsByCityAndSpeciality(
-                Doctor.City.valueOf(patient.getCity().toUpperCase()), requiredSpeciality
-        );
+        try {
+            List<Doctor> doctors = doctorService.getDoctorsByCityAndSpeciality(
+                    Doctor.City.valueOf(patient.getCity().toUpperCase()), requiredSpeciality
+            );
 
-        if (doctors.isEmpty()) {
-            throw new NoDoctorFoundException("There isn't any doctor present at your location for your symptom");
+            if (doctors.isEmpty()) {
+                throw new NoDoctorFoundException("There isn't any doctor present at your location for your symptom");
+            }
+
+            return doctors;
+        } catch (IllegalArgumentException e) {
+            throw new InvalidCityException(e.getMessage());
         }
-
-        return doctors;
     }
 }
